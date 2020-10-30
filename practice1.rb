49107d38017e8da20b855d8c4eaf5edd265bffe4
shane <shanecolbyzero8@gmail.com>
2020-10-29 20:32:38 -0500

    used interpolation to present covid deaths

--- practice1.rb
+++ practice1.rb
@@ -4,3 +4,4 @@ covid_info = HTTP.get("https://data.cityofnewyork.us/resource/rc75-m7u3.json").p
 p covid_info[0]["case_count"]
 p covid_info[5]["case_count"]
 p covid_info[19]["death_count"]
+p "They had #{covid_info[19]["death_count"]} deaths."
