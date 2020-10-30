4d648c7f9e6eb396d0dd8f0943256b7d759a7d69
shane <shanecolbyzero8@gmail.com>
2020-10-29 21:41:46 -0500

    hint on how to use variable to make strings

--- practice1.rb
+++ practice1.rb
@@ -6,4 +6,13 @@ p covid_info[5]["case_count"]
 p covid_info[19]["death_count"]
 p "They had #{covid_info[19]["death_count"]} deaths."
 
-p "The combined cases are #{covid_info[0]["case_count"]} +"
+p "The combined cases are #{covid_info[0]["case_count"]} + #{covid_info[5]["case_count"]"
+
+# make variable and use the variable with inperpolation to string sentences with data
+
+
+
+
+
+
+
