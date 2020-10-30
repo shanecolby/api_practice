500855651375202410dcc569bb94e993196b8f15
shane <shanecolbyzero8@gmail.com>
2020-10-29 20:29:37 -0500

    printed out case counts and death cout for different dates for covid

--- practice1.rb
+++ practice1.rb
@@ -2,3 +2,5 @@ require "http"
 covid_info = HTTP.get("https://data.cityofnewyork.us/resource/rc75-m7u3.json").parse
 
 p covid_info[0]["case_count"]
+p covid_info[5]["case_count"]
+p covid_info[19]["death_count"]
